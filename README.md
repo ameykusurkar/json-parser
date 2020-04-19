1dc9396a1455ed7e34632db5a795ba30971976f9
Amey Kusurkar <amey1000@gmail.com>
2020-04-19 21:04:15 +0100

    Some minor refactoring

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # json-parser
-A JSON parser written in Haskell using parser combinators. Done as an educational exercise to learn about [parser combinators](https://en.wikipedia.org/wiki/Parser_combinator).
+A JSON parser written in Haskell from scratch using no dependencies. Done mostly as an educational exercise to learn about [parser combinators](https://en.wikipedia.org/wiki/Parser_combinator).
 
 ### Usage
 
--- json_parser.hs
+++ json_parser.hs
@@ -4,50 +4,56 @@ import Data.Char
 main = interact (show . fmap fst . runParser jsonParser)
 
 data Json
- = JNull
- | JBool Bool
- | JNumber Int
- | JString String
- | JArray [Json]
- | JObject [(String, Json)]
+ = JNull | JBool Bool | JNumber Int | JString String
+ | JArray [Json] | JObject [(String, Json)]
  deriving (Show, Eq)
 
+-- A parser that given an input string, consumes from it until it can form a value of
+-- type `a`, and returns the remaining input wrapped in a Maybe. If it fails to form
+-- an `a`, it returns Nothing. The idea is to build composable parsers that each look
+-- for and form a specific value, and chain them.
 newtype Parser a = Parser {
   runParser :: String -> Maybe (a, String)
 }
 
 instance Functor Parser where
   fmap f (Parser p) = Parser q
-    where q input = fmap g (p input)
-          g (y, rest) = (f y, rest)
+    where q input = (p input) >>= applyF
+          applyF (parsed, rest) = Just (f parsed, rest)
 
+-- This is the core piece of logic that allows us to chain parsers. Implementing <*>
+-- allows us to chain parsers together by passing the unused input from one parser to
+-- the next one as input, and combine their parsed values.
 instance Applicative Parser where
   pure x = Parser (\input -> Just (x, input))
   (Parser p) <*> (Parser q) = Parser r
-    where r input = (p input) >>= runSecond
-          runSecond (y, rest) = (q rest) >>= \(y', rest') -> Just (y y', rest')
+    where r input = do (parsedF, rest) <- p input
+                       (parsed, rest') <- q rest
+                       return (parsedF parsed, rest')
 
 instance Alternative Parser where
-  empty = Parser (\_ -> Nothing)
+  empty = Parser (const Nothing)
   (Parser p) <|> (Parser q) = Parser (\input -> p input <|> q input)
+
+-- Some helper parsers --
 
 notNull :: Parser [a] -> Parser [a]
 notNull (Parser f) = Parser p
   where p input = (f input) >>= fail
-        fail (ys, rest) = if null ys then Nothing else Just (ys, rest)
+        fail (parsed, rest) = if null parsed then Nothing else Just (parsed, rest)
 
 charParser :: Char -> Parser Char
 charParser c = Parser f
   where f "" = Nothing
         f (c':cs)
-          | c == c' = Just (c, cs)
+          | c == c'   = Just (c, cs)
           | otherwise = Nothing
 
 stringParser :: String -> Parser String
 stringParser = sequenceA . map charParser
 
 spanParser :: (Char -> Bool) -> Parser String
-spanParser f = Parser (\input -> Just (span f input))
+spanParser f = Parser (Just . span f)
 
 separatedBy :: Parser String -> Parser a -> Parser [a]
 separatedBy sep item = (:) <$> item <*> many (sep *> item) <|> pure []
@@ -58,9 +64,10 @@ wsParser = many (charParser ' ')
 stringLitParser :: Parser String
 stringLitParser = charParser '"' *> spanParser (/= '"') <* charParser '"'
 
--- Caveats:
--- Cannot parse strings with escaped characters
--- Can only parse numbers in integer form
+-- The parsing logic --
+
+-- TODO: Parse string literals with escaped characters
+--       Parse floating point numbers
 jsonParser :: Parser Json
 jsonParser = wsParser *>
   (jNullParser <|> jBoolParser <|> jNumberParser <|> jStringParser <|> jArrayParser <|> jObjectParser)
@@ -80,6 +87,8 @@ jNumberParser = (JNumber . read) <$> notNull (spanParser isDigit)
 jStringParser :: Parser Json
 jStringParser = JString <$> stringLitParser
 
+-- We can see here how small parsers can be chained to parse an array. We try to parse a
+-- '[' character, followed one or many JSON items separated by a ',' and finally a '].
 jArrayParser :: Parser Json
 jArrayParser = JArray <$> (charParser '[' *> itemsParser <* charParser ']')
   where itemsParser = separatedBy (stringParser ",") jsonParser
